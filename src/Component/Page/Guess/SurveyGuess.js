45722a4e06b3699328f7cb3c1b2add24417fee63
yakapi <realitygameserver@gmail.com>
2022-05-01 20:59:52 +0200

    last stape before finish guest survey form

--- src/Component/Page/Guess/SurveyGuess.js
+++ src/Component/Page/Guess/SurveyGuess.js
@@ -9,9 +9,13 @@ class SurveyGuess extends React.Component{
     }
   }
   componentDidMount(){
-    let url_survey = this.props.location.pathname
+    function isEmpty(str) {
+      return (!str || str.length === 0 );
+    }let url_survey = this.props.location.pathname
     let array_uri = url_survey.split(';')
     let answer_string = array_uri[2]
+    if (!isEmpty(answer_string)) {
+
     let answer_array = answer_string.split("%20")
     let answer = ""
     for (var i = 0; i < answer_array.length; i++) {
@@ -19,10 +23,12 @@ class SurveyGuess extends React.Component{
     }
     let survey_array = url_survey.split("/")
     let survey = survey_array[2]
+    let survey_clean = survey.replaceAll("%20", " ")+"?"
     let data_to_send = {
       "call": "exist_survey",
-      "survey": survey
+      "survey": survey_clean
     }
+    //   this.setState({"exist": false})
     fetch('http://woogo-api.victorbarlier.fr/guess.php',{
       method: 'post',
       credentials: 'include',
@@ -33,14 +39,15 @@ class SurveyGuess extends React.Component{
       body: Object.entries(data_to_send).map(([k,v])=>{return k+'='+v}).join('&')
     }).then(response => response.json()).then(data => {
       console.log(data);
-      // if (data.success) {
-      //   this.setState({"content": answer})
-      //   this.setState({"exist": true})
-      // }else {
-      //   this.setState({"exist": false})
-      // }
+      if (data.success) {
+        this.setState({"content": answer})
+        this.setState({"exist": true})
+      }else {
+        this.setState({"exist": false})
+      }
+
     })
-    console.log(answer);
+  }
   }
   render(){
     return(
@@ -53,6 +60,34 @@ class SurveyGuess extends React.Component{
 export default SurveyGuess
 
 class SurveyView extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      "response_state": false,
+      "response_content": [],
+      "error": null
+    }
+  }
+  rstape_one = (e) => {
+    e.preventDefault()
+    let stape_value
+    let stape_unchecked = 0
+    for (var i = 0; i < e.target.length - 1; i++) {
+      if (e.target[i].checked) {
+        stape_value = e.target[i].value
+      }else {
+        stape_unchecked += 1
+      }
+    }
+    if (stape_unchecked == 3) {
+      this.setState({"error": "Choisissez un champ"})
+    }else {
+      let n_array = []
+      n_array.push(stape_value)
+      this.setState({"response_content": n_array})
+      this.setState({"response_state": true})
+    }
+  }
   render(){
     return(
       <div className="GuessBoard">
@@ -90,29 +125,52 @@ class SurveyView extends React.Component{
                    </div>
                </div>
            </div>
-           <div className="GuessBox">
-             <form>
-               <label htmlFor="guest_yes" className="radio_guest_survey">
-                 <input type="radio" name="guest_response" id="guest_yes"/>
-                 <p>Oui</p>
-               </label>
-               <label htmlFor="guest_no" className="radio_guest_survey">
-                 <input type="radio" name="guest_response" id="guest_no"/>
-                 <p>Non</p>
-               </label>
-               <label htmlFor="guest_maybe" className="radio_guest_survey">
-                 <input type="radio" name="guest_response" id="guest_maybe"/>
-                 <p>Peut être</p>
-               </label>
-               <div className="sub_guest_survey">
-                 <input className="btnBoard" type="submit" name="sub_guest_survey" value="Valider"/>
-               </div>
-             </form>
+           <div className="GuessBox rsrv">
+           {this.state.response_state ? <GuestName/> : <GuestVote error={this.state.error} rstape={this.rstape_one}/>}
            </div>
          </div>
        </div>
        <p className="copyright cGuess">Copyright WebProvide 2022</p>
       </div>
+    )
+  }
+}
+class GuestName extends React.Component{
+  render(){
+    return(
+      <div>
+      <form className="ResultForm">
+      <h2 className="ResultTitle">Votre Nom</h2>
+        <input className="putSond nsurv" type="text" pattern="[ a-zA-Z0-9,#.-]+" minLength="4" maxLength="10" />
+        <div className="validate_survey">
+          <input className="btnBoard" value="Valider" type="submit"/>
+        </div>
+      </form>
+      </div>
+    )
+  }
+}
+class GuestVote extends React.Component{
+  render(){
+    return(
+      <form onSubmit={this.props.rstape}>
+        <label htmlFor="guest_yes" className="radio_guest_survey">
+          <input type="radio" name="guest_response" value="yes" id="guest_yes"/>
+          <p>Oui</p>
+        </label>
+        <label htmlFor="guest_no" className="radio_guest_survey">
+          <input type="radio" name="guest_response" value="no" id="guest_no"/>
+          <p>Non</p>
+        </label>
+        <label htmlFor="guest_maybe" className="radio_guest_survey">
+          <input type="radio" name="guest_response" value="maybe" id="guest_maybe"/>
+          <p>Peut être</p>
+        </label>
+        <div className="sub_guest_survey">
+          <p>{this.props.error}</p>
+          <input className="btnBoard" type="submit" name="sub_guest_survey" value="Valider"/>
+        </div>
+      </form>
     )
   }
 }
@@ -124,7 +182,7 @@ class ReturnHome extends React.Component{
        <div className="SurveyBlock">
          <div className="SurveyBox">
            <div className="GuessBox specm">
-             <h2 className="ResultTitle">Le Sondage n'existe pas</h2>
+             <h2 className="ResultTitle">Le Sondage n'existe plus</h2>
            </div>
          </div>
        </div>
