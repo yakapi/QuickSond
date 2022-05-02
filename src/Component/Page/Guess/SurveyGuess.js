306e31dc5f25fa5c99f000cffd4c91e8199fbf4b
yakapi <realitygameserver@gmail.com>
2022-05-02 14:10:41 +0200

    result survey started

--- src/Component/Page/Guess/SurveyGuess.js
+++ src/Component/Page/Guess/SurveyGuess.js
@@ -52,7 +52,7 @@ class SurveyGuess extends React.Component{
   render(){
     return(
       <div>
-        {this.state.exist ? <SurveyView content={this.state.content} /> : <ReturnHome/>}
+        {this.state.exist ? <SurveyView location={this.props.location} content={this.state.content} /> : <ReturnHome/>}
       </div>
     )
   }
@@ -65,7 +65,8 @@ class SurveyView extends React.Component{
     this.state = {
       "response_state": false,
       "response_content": [],
-      "error": null
+      "error": null,
+      "result_content": []
     }
   }
   rstape_one = (e) => {
@@ -88,6 +89,80 @@ class SurveyView extends React.Component{
       this.setState({"response_state": true})
     }
   }
+  rstape_two = (e) => {
+    e.preventDefault()
+    function isEmpty(str) {
+      return (!str || str.length === 0 );
+    }
+    let survey_clean
+    let url_survey = this.props.location.pathname
+    let array_uri = url_survey.split(';')
+    let answer_string = array_uri[2]
+    if (!isEmpty(answer_string)) {
+
+    let answer_array = answer_string.split("%20")
+    let answer = ""
+    for (var i = 0; i < answer_array.length; i++) {
+          answer += answer_array[i]+" "
+    }
+    let survey_array = url_survey.split("/")
+    let survey = survey_array[2]
+    survey_clean = survey.replaceAll("%20", " ")+"?"
+    }
+    let data_to_send = {
+      "call": "add_result",
+      "result": this.state.response_content[0],
+      "name": e.target[0].value,
+      "type": survey_clean
+    }
+    console.log(data_to_send);
+    //   this.setState({"exist": false})
+    fetch('http://woogo-api.victorbarlier.fr/guess.php',{
+      method: 'post',
+      credentials: 'include',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: Object.entries(data_to_send).map(([k,v])=>{return k+'='+v}).join('&')
+    }).then(response => response.json()).then(data => {
+      console.log(data);
+      if (data.success) {
+        this.setState({"response_state": false})
+        console.log(data_to_send);
+        let result_content = this.state.result_content
+        let n_result_content = []
+        n_result_content.push(data_to_send.type)
+        n_result_content.push(data_to_send.name)
+        n_result_content.push(data_to_send.result)
+        result_content.push(n_result_content)
+        this.setState({"result_content": result_content})
+        console.log(this.state.result_content);
+      }
+    })
+  }
+  componentDidMount(){
+    // Récupérer tous les resultat du sondage
+    function isEmpty(str) {
+      return (!str || str.length === 0 );
+    }
+    let survey_clean
+    let url_survey = this.props.location.pathname
+    let array_uri = url_survey.split(';')
+    let answer_string = array_uri[2]
+    if (!isEmpty(answer_string)) {
+
+    let answer_array = answer_string.split("%20")
+    let answer = ""
+    for (var i = 0; i < answer_array.length; i++) {
+          answer += answer_array[i]+" "
+    }
+    let survey_array = url_survey.split("/")
+    let survey = survey_array[2]
+    survey_clean = survey.replaceAll("%20", " ")+"?"
+    }
+    console.log(survey_clean);
+  }
   render(){
     return(
       <div className="GuessBoard">
@@ -96,37 +171,10 @@ class SurveyView extends React.Component{
          <div className="SurveyBox">
            <h1 className="SurveyTitle">{this.props.content}?</h1>
            <div className="GuessBox specm">
-             <h2 className="ResultTitle">Résultat</h2>
-               <div className="ResultProgress">
-                 <div className="ProgressSurveyBord">
-                   <div className="ProgressSurvey yesway" style={{width: "100px"}}></div>
-                 </div>
-                  <p>👍</p>
-                  <div className="nb_result">
-                    <p>15</p>
-                  </div>
-               </div>
-               <div className="ResultProgress">
-                 <div className="ProgressSurveyBord">
-                   <div className="ProgressSurvey noway"></div>
-                 </div>
-                 <p>👎</p>
-                   <div className="nb_result">
-                     <p>15</p>
-                   </div>
-               </div>
-               <div className="ResultProgress">
-                 <div className="ProgressSurveyBord">
-                   <div className="ProgressSurvey maybe"></div>
-                 </div>
-                 <p>🤷‍♂️</p>
-                   <div className="nb_result">
-                     <p>15</p>
-                   </div>
-               </div>
+             <ResultSurvey result={this.state.result_content}/>
            </div>
            <div className="GuessBox rsrv">
-           {this.state.response_state ? <GuestName/> : <GuestVote error={this.state.error} rstape={this.rstape_one}/>}
+           {this.state.response_state ? <GuestName rstape={this.rstape_two}/> : <GuestVote error={this.state.error} rstape={this.rstape_one} />}
            </div>
          </div>
        </div>
@@ -135,11 +183,55 @@ class SurveyView extends React.Component{
     )
   }
 }
+class ResultSurvey extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      "yes": [0,"0px",[]],
+      "no": [0,"0px",[]],
+      "maybe": [0,"0px",[]],
+    }
+  }
+  render(){
+    return(
+      <div>
+        <h2 className="ResultTitle">Résultat</h2>
+        <div className="ResultProgress">
+          <div className="ProgressSurveyBord">
+            <div className="ProgressSurvey yesway" style={{width: this.state.yes[1]}}></div>
+          </div>
+          <p>👍</p>
+          <div className="nb_result">
+            <p>{this.state.yes[0]}</p>
+          </div>
+        </div>
+        <div className="ResultProgress">
+          <div className="ProgressSurveyBord">
+            <div className="ProgressSurvey noway" style={{width: this.state.no[1]}}></div>
+          </div>
+          <p>👎</p>
+          <div className="nb_result">
+            <p>{this.state.no[0]}</p>
+          </div>
+        </div>
+        <div className="ResultProgress">
+          <div className="ProgressSurveyBord">
+            <div className="ProgressSurvey maybe" style={{width: this.state.maybe[1]}}></div>
+          </div>
+          <p>🤷‍♂️</p>
+          <div className="nb_result">
+            <p>{this.state.maybe[0]}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 class GuestName extends React.Component{
   render(){
     return(
       <div>
-      <form className="ResultForm">
+      <form className="ResultForm" onSubmit={this.props.rstape}>
       <h2 className="ResultTitle">Votre Nom</h2>
         <input className="putSond nsurv" type="text" pattern="[ a-zA-Z0-9,#.-]+" minLength="4" maxLength="10" />
         <div className="validate_survey">
